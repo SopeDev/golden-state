ccd1e4b56beba7d1d1c319eee376bd585a562c7a
Jorge Carlos Quevedo <jorgecarlosquevedo@gmail.com>
2025-06-23 11:20:19 -0700

    added login via credentials

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,50 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { compare } from 'bcryptjs'
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 export const authOptions = {
+  session: {
+    strategy: 'jwt'
+  },
   // Configure one or more authentication providers
   providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials, req) {
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        })
+
+        if (user) {
+          const passwordCorrect = await compare(credentials.password, user.password)
+
+          if (passwordCorrect) {
+            return {
+              id: user.id,
+              email: user.email
+            }
+          }
+
+          return null
+        }
+
+        return null
+      }
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-    }),
-    // ...add more providers here
+    })
   ],
 }
 
---
+++ src/app/api/auth/register/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+import { hash } from 'bcryptjs'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export async function POST(request) {
+	try {
+		const { email, password } = await request.json()
+		//validate email and password
+
+		const hashedPassword = await hash(password, 10)
+
+		const newUser = await prisma.user.create({
+			data: {
+				email,
+				password: hashedPassword
+			}
+		})
+
+		console.log({email, password})
+	} catch(e) {
+		console.log({e})
+	}
+
+	return NextResponse.json({ message: 'success' })
+}
