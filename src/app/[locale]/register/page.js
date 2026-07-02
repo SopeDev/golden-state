60d18e0efe51aaf1553ac56265f26729c0239a1c
Jorge Carlos Quevedo <jorgecarlosquevedo@gmail.com>
2026-07-02 10:46:01 -0700

    small ui fix

--- src/app/[locale]/register/page.js
+++ src/app/[locale]/register/page.js
@@ -1,7 +1,14 @@
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { redirect } from '@/i18n/navigation'
 import Form from './form'
 
 export default async function RegisterPage() {
-	return (
-		<Form />
-	)
+  const session = await getServerSession(authOptions)
+
+  if (session?.user) {
+    await redirect('/dashboard')
+  }
+
+  return <Form />
 }
